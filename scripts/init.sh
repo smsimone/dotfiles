6e787b880b30a859637cec3136a9a2d9ac65ef6c
Simone Masoero <smsimone65@gmail.com>
2023-11-18 10:29:56 +0100

    feature: added export to starship config

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,6 +3,10 @@
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
+if [ -f "$HOME/.config/starship.toml" ]; then
+	export STARSHIP_CONFIG="$HOME/.config/starship.toml"
+fi
+
 source "$(dirname "$0")/path.sh"
 source "$(dirname "$0")/aliases.sh"
 source "$(dirname "$0")/scripts.sh"
