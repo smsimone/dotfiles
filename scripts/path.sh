81df3eb7fb548fc42749d8a25d9cc4cfdf0f46d6
Simone Masoero <smsimone65@gmail.com>
2023-11-28 23:02:52 +0100

    feature: removed paths debug print

--- scripts/path.sh
+++ scripts/path.sh
@@ -2,44 +2,43 @@
 
 export GO_PATH=~/go
 
-IFS=":" read -rA current_paths <<< "$PATH"
+IFS=":" read -rA current_paths <<<"$PATH"
 
 paths=(
-  "$GO_PATH/bin"
-  "$HOME/.local/bin"
-  "$HOME/.pub-cache/bin"
-  "$HOME/.pyenv/bin"
-  "$HOME/.scripts"
-  "$HOME/.spicetify"
-  "$HOME/Development/monarch/bin"
-  "$HOME/Library/Android/sdk/tools/bin"
-  "$HOME/Library/Python/3.11/bin"
-  "$HOME/fvm/default/bin"
-  "/bin"
-  "/opt/homebrew/bin"
-  "/sbin"
-  "/usr/bin"
-  "/usr/local/bin/"
-  "/usr/local/opt/libpq/bin"
-  "/usr/local/sbin"
-  "/usr/sbin"
+	"$GO_PATH/bin"
+	"$HOME/.local/bin"
+	"$HOME/.pub-cache/bin"
+	"$HOME/.pyenv/bin"
+	"$HOME/.scripts"
+	"$HOME/.spicetify"
+	"$HOME/Development/monarch/bin"
+	"$HOME/Library/Android/sdk/tools/bin"
+	"$HOME/Library/Python/3.11/bin"
+	"$HOME/fvm/default/bin"
+	"/bin"
+	"/opt/homebrew/bin"
+	"/sbin"
+	"/usr/bin"
+	"/usr/local/bin/"
+	"/usr/local/opt/libpq/bin"
+	"/usr/local/sbin"
+	"/usr/sbin"
 )
 
 paths_combined=""
 for path_item in "${paths[@]}"; do
-  if [ -z "${paths_combined}" ]; then
-    found=false
-    case "${current_paths[@]}" in *"$path_item"*) found=true;; esac 
+	if [ -z "${paths_combined}" ]; then
+		found=false
+		case "${current_paths[@]}" in *"$path_item"*) found=true ;; esac
 
-    if ! "$found"; then 
-      paths_combined="$path_item"
-    fi
-  else
-    if [ ! -d "${path_item}" ]; then
-      echo "path '${path_item}' does not exists"
-    fi
-    paths_combined="${paths_combined}:${path_item}"
-  fi
+		if ! "$found"; then
+			paths_combined="$path_item"
+		fi
+	else
+		if [ -d "${path_item}" ]; then
+			paths_combined="${paths_combined}:${path_item}"
+		fi
+	fi
 done
 
 export PATH="$PATH:${paths_combined}"
