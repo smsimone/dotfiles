37724443839b9d647536992ca6ce2c6f12062683
smsimone <smsimone65@gmail.com>
2024-02-14 21:54:32 +0100

    refactor: random edits which I don't remember

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,19 +1,17 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.3",
-	event = "VeryLazy",
-	dependencies = "nvim-lua/plenary.nvim",
-	config = function()
-		local telescope = require("telescope")
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.3",
+  event = "VeryLazy",
+  dependencies = "nvim-lua/plenary.nvim",
+  config = function()
+    local telescope = require("telescope")
 
-		telescope.setup({
-			pickers = {
-				colorscheme = {
-					enable_preview = true,
-				},
-			},
-		})
-
-		--telescope.load_extension("notify")
-	end,
+    telescope.setup({
+      pickers = {
+        colorscheme = {
+          enable_preview = true,
+        },
+      },
+    })
+  end,
 }
