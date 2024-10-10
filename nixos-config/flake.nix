9d8687b5b1c7b3ce7e8d5018cfc39d621b0d50a5
Simone Masoero <smsimone65@gmail.com>
2024-10-10 22:50:58 +0200

    first nix config

---
+++ nixos-config/flake.nix
@@ -0,0 +1,32 @@
+{
+    description = "Nix configuration";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs";
+
+        nix-darwin.url = "github:lnl7/nix-darwin/master";
+        nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+        home-manager.url = "github:nix-community/home-manager";
+        home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    outputs = inputs @ { self, ... }: let
+        nixpkgsConfig = {
+            config.allowUnfree = true;
+        };
+    in {
+        darwinConfigurations = let 
+            inherit (inputs.nix-darwin.lib) darwinSystem; 
+        in{
+            machine = darwinSystem{
+system = "aarch64-darwin";
+specialArgs = {inherit inputs; };
+
+modules = [
+   ./configuration.nix
+];
+            };
+        };
+    };
+}
