1c053fe9220a6f47b60f0dc160984ea792f77966
Simone Masoero <smsimone65@gmail.com>
2024-10-21 17:11:24 +0200

    automatic theme based on system appearance

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,8 +1,23 @@
 local wezterm = require('wezterm')
 local config = wezterm.config_builder()
 
+local function get_appearance()
+	if wezterm.gui then
+		return wezterm.gui.get_appearance()
+	end
+	return "Dark"
+end
+
+local function scheme_for_appearance(appearance)
+	if appearance:find 'Dark' then
+		return 'catppuccin-mocha'
+	else
+		return 'catppuccin-latte'
+	end
+end
+
 config.font = wezterm.font 'Iosevka Nerd Font'
 config.window_decorations = "RESIZE"
-config.color_scheme = "catppuccin-mocha"
+config.color_scheme = scheme_for_appearance(get_appearance())
 
 return config
