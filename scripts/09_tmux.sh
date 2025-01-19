d0f45d6345cc5e17eda26277f4ba1de2e1b3f35e
Simone Masoero <smsimone65@gmail.com>
2025-01-19 14:36:03 +0100

    automatically change statusbar theme depending on os theme

--- scripts/09_tmux.sh
+++ scripts/09_tmux.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 SOURCE_CONFIG_FOLDER="$SCRIPT_DIR/../configs/tmux"
 TARGET_CONFIG_FOLDER="$HOME/.config/tmux"
@@ -20,3 +22,27 @@ if [ ! -d "$TARGET_CONFIG_FOLDER" ]; then
 		source-file ~/.config/tmux/tmux.conf
 	fi
 fi
+
+function apply_theme(){
+	local from="$1"
+	local to="$2"
+
+	exists=$(grep -w "$from" "$SOURCE_CONFIG_FOLDER/statusbar.conf")
+	if [ ! -z "$exists" ]; then
+		sed -ie "s/$from/$to/g" "$SOURCE_CONFIG_FOLDER/statusbar.conf"
+		return 0
+	fi
+	return 1
+}
+
+brightness=$(defaults read -g AppleInterfaceStyle 2>&1 | tr -d '\n')
+if [[ "$brightness" == "Dark" ]]; then
+	apply_theme 'latte' 'mocha' && tmux kill-server
+else
+	apply_theme 'mocha' 'latte' && tmux kill-server
+fi
+
+if [ -f "$SOURCE_CONFIG_FOLDER/statusbar.confe" ]; then
+	rm "$SOURCE_CONFIG_FOLDER/statusbar.confe"
+fi
+
