3ccbf83c99baf7aa0253421762c48408288c3b31
Simone Masoero <antlia@mbp-antlia.local>
2025-01-22 09:38:50 +0100

    made some changes to alacritty config

--- scripts/05_alacritty.sh
+++ scripts/05_alacritty.sh
@@ -1,15 +1,14 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 SOURCE_CONFIG_FOLDER="$SCRIPT_DIR/../configs/alacritty"
 TARGET_CONFIG_FOLDER="$HOME/.config/alacritty"
 
-if [ ! -d "$TARGET_CONFIG_FOLDER" ]; then 
+if [ ! -d "$TARGET_CONFIG_FOLDER" ]; then
 	ln -s "$SOURCE_CONFIG_FOLDER" "$TARGET_CONFIG_FOLDER"
 fi
 
-apply_theme()
-{
+apply_theme() {
 	local theme_name="$1"
 	if [ ! -f "$TARGET_CONFIG_FOLDER/catppuccin-${theme_name}.toml" ]; then
 		curl -LO --output-dir "$TARGET_CONFIG_FOLDER" "https://github.com/catppuccin/alacritty/raw/main/catppuccin-${theme_name}.toml"
@@ -17,6 +16,7 @@ apply_theme()
 
 	sanitized=$(echo "$TARGET_CONFIG_FOLDER/catppuccin-${theme_name}.toml" | sed 's/\//\\\//g')
 	sed -ie "s/^import.*/import=[\"$sanitized\"]/" "$SOURCE_CONFIG_FOLDER/alacritty.toml"
+	if [ -f "$SOURCE_CONFIG_FOLDER/alacritty.tomle" ]; then rm "$SOURCE_CONFIG_FOLDER/alacritty.tomle"; fi
 }
 
 brightness=$(defaults read -g AppleInterfaceStyle 2>&1 | tr -d '\n')
@@ -25,4 +25,3 @@ if [[ "$brightness" == "Dark" ]]; then
 else
 	apply_theme 'latte'
 fi
-
