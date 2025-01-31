e7bba0f9d370306955cc451d693bdbae17fa0d12
Simone Masoero <smsimone65@gmail.com>
2025-01-31 17:51:13 +0100

    changed the way tmux is disabled on startup

--- init.sh
+++ init.sh
@@ -5,11 +5,9 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 export STARSHIP_CONFIG="$HOME/.config/starship.toml"
 export STARSHIP_CONFIG="$SCRIPT_DIR/configs/starship.toml"
 
-if [[ "$ENABLE_TMUX" == "1" ]]; then
-	if [[ "$TERM_PROGRAM" == "vscode" ]] || [[ "$TERM_PROGRAM" == "ghostty" ]] || [[ "$TERM_PROGRAM" == "Apple_Terminal" ]]; then
-		# Avoid using tmux in vscode
-		export TMUX_AUTOSTART=false
-	fi
+if [[ "$ENABLE_TMUX" == "0" ]] || [[ "$TERM_PROGRAM" == "vscode" ]] || [[ "$TERM_PROGRAM" == "ghostty" ]] || [[ "$TERM_PROGRAM" == "Apple_Terminal" ]]; then
+	# Avoid using tmux in vscode
+	export TMUX_AUTOSTART=false
 fi
 
 find "$SCRIPT_DIR/scripts" -name '*.sh' | sort | while read -r script; do
