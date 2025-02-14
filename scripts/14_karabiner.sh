ec530f44b86e51d2545d5504d175dcf0a014f1bc
Simone Masoero <antlia@mbp-antlia.local>
2025-02-14 14:24:35 +0100

    added karabiner mappings

--- scripts/14_karabiner.sh
+++ scripts/14_karabiner.sh
@@ -2,6 +2,6 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 TARGET_CONFIG_DIR="$HOME/.config/karabiner"
 SOURCE_CONFIG_DIR="$SCRIPT_DIR/../configs/karabiner"
 
-if [ ! -f "$TARGET_CONFIG_DIR" ]; then
+if [ ! -d "$TARGET_CONFIG_DIR" ]; then
 	ln -s "$SOURCE_CONFIG_DIR" "$TARGET_CONFIG_DIR"
 fi
